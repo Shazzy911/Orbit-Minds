09acb59cc744e066ad4fdee2355c3157059f6906
Shazzy911 <Shazzydev911@gmail.com>
2024-08-21 22:58:22 +0500

    fixing/deployment

--- src/app/(pages)/featured/[feature]/page.tsx
+++ src/app/(pages)/featured/[feature]/page.tsx
@@ -1,41 +1,43 @@
 
-// import Details from "@/components/layout/details_interface/Details";
-// import { postProps } from "@/types/postList";
+import Details from "@/components/layout/details_interface/Details";
+import { postProps } from "@/types/postList";
 
 
-// const getData = async ( id: string | number )=> {
-//   try {
-//     let response = await fetch(`http://localhost:3000/api/featured/${id}`, {
-//       cache: "default",
-//     })
+const getData = async ( id: string | number )=> {
 
-//     let data = await response.json();
-//     return data;
-//   } catch (error) {
-//     throw new Error("Failed to Fetch Data by Id");
-//   }
-// }
+    const apiURL = process.env.API_URL;
+  try {
+    let response = await fetch(`${apiURL}/api/featured/${id}`, {
+      cache: "default",
+    })
 
-// type Data = {
-//   params: {
-//     feature: string | number | (string | number)[];
-//   };
-// };
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("Failed to Fetch Data by Id");
+  }
+}
 
-// const Page = async ({ params }: Data) => {
-//   const id: string | number | any = params.feature;
-//   let post: postProps = await getData(id);
-//   return (
-//     <>
-//       <Details post={post}/>
-//     </>
-//   );
-// };
+type Data = {
+  params: {
+    feature: string | number | (string | number)[];
+  };
+};
 
-// export default Page;
-// export function generateMetadata () { 
-//   return{
-//     title: "Galaxy Blog - Feature Category ",
-//     description: "This is the galaxy blog, it contains information about the developer and owner of  website",
-//   }
-// }
+const Page = async ({ params }: Data) => {
+  const id: string | number | any = params.feature;
+  let post: postProps = await getData(id);
+  return (
+    <>
+      <Details post={post}/>
+    </>
+  );
+};
+
+export default Page;
+export function generateMetadata () { 
+  return{
+    title: "Galaxy Blog - Feature Category ",
+    description: "This is the galaxy blog, it contains information about the developer and owner of  website",
+  }
+}
